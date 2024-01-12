a16895539a5e6215af9ae4f8ee337d8bde6ddf4c
Nurdaulet <abdikaimovnurdaulet@gmail.com>
2024-01-13 01:19:09 +0600

    Добавлены изменения * Изменен тип дефолтного шрифта с bold на semibold как в figma * Добавлен cityTitle и стрелка указывающая вниз

--- testTask/testTask/MainModule/MainView/MainViewController.swift
+++ testTask/testTask/MainModule/MainView/MainViewController.swift
@@ -11,6 +11,23 @@ import SnapKit
 final class MainViewController: UIViewController {
     private let presenter: MainPresenter
     
+    //UI Elements
+    private let cityTitle: UILabel = {
+        let title = UILabel()
+        title.font = UIFont.defaultBoldFont
+        title.textColor = .black
+        title.text = "Moscow"
+        return title
+    }()
+    
+    private let arrowImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "arrow.down")
+        image.contentMode = .scaleAspectFit
+        image.backgroundColor = .clear
+        return image
+    }()
+    
     // LifeCycle
     init(presenter: MainPresenter) {
         self.presenter = presenter
@@ -23,7 +40,25 @@ final class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        setupViews()
+    }
+    
+    private func setupViews() {
         view.backgroundColor = .white
+        
+        view.addSubview(cityTitle)
+        cityTitle.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+        }
+        
+        view.addSubview(arrowImageView)
+        arrowImageView.snp.makeConstraints { make in
+            make.leading.equalTo(cityTitle.snp.leading).offset(70)
+            make.size.equalTo(14)
+            make.centerY.equalTo(cityTitle.snp.centerY)
+        }
     }
 }
 
--- testTask/testTask/TabBar/TabBarController.swift
+++ testTask/testTask/TabBar/TabBarController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
